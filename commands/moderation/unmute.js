e5a2a149386a1c14b0a685285247a8ca993e06f6
blázs <64538277+blazsmaster@users.noreply.github.com>
2021-03-23 22:24:31 +0100

    Create unmute.js
    Subfolder

---
+++ commands/moderation/unmute.js
@@ -0,0 +1,84 @@
+// npmjs packages
+const Discord = require('discord.js');
+
+// configuration
+const config = require('../../config.json');
+
+// export command
+module.exports = {
+    
+    // command name
+	name: 'unmute',
+
+    // command description
+	description: 'Unmute a member in the server.',
+
+    // guild only
+    guildOnly: true,
+
+    // perms
+	permissions: 'KICK_MEMBERS',
+
+    // command
+	execute(message) {
+
+        // get member
+        const member = message.mentions.members.first();
+
+        // no member
+        if (!member) {
+
+            // send message to channel
+            message.channel.send(
+
+                // embed
+                new Discord.MessageEmbed()
+                .setColor(config.color.red)
+                .setTitle('Missing args!')
+                .setDescription('You need to mention a member to mute!')
+                .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+                .setTimestamp()
+            );
+
+            // cancel
+            return;
+        };
+
+        // mute role
+        let role = message.guild.roles.cache.find(r => r.name === 'Muted');
+
+        // if no role
+        if (!role) {
+
+            // send message to channel
+            message.channel.send(
+
+                // embed
+                new Discord.MessageEmbed()
+                .setColor(config.color.red)
+                .setTitle('Unmute error!')
+                .setDescription('I can not find the `Muted` role!')
+                .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+                .setTimestamp()
+            );
+
+            // cancel
+            return;
+        };
+
+        // mute member
+        member.roles.remove(role);
+
+        // send message to channel
+        message.channel.send(
+
+            // embed
+            new Discord.MessageEmbed()
+            .setColor(config.color.green)
+            .setTitle(`Successful unmute!`)
+            .setDescription(`${member} got unmuted by ${message.author}!`)
+            .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+            .setTimestamp()
+        );
+	},
+};
